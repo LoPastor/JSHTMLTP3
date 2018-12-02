5aad6322c069b72968843bb8bb721cb7f2fea43a
LoPastor <44533866+LoPastor@users.noreply.github.com>
2018-12-03 00:20:11 +0100

    Add files via upload
    PUT/POST/DELETE géré
    Pagination première page/page précédente gérée avec impossibilité d'aller avant la première page
    Page suivante gérée
    Dernière page non gérée
    Recherche sur le nom effectuée avec la fonction _.debounce.

---
+++ client/js/script.js
@@ -0,0 +1,227 @@
+window.onload=init;
+
+function init() {
+    new Vue({
+        el: "#app",
+        data: {
+            restaurants: [
+                {
+                   /* nom: 'café de Paris',
+                    cuisine: 'Française'
+                },
+                {
+                    nom: 'café de Paris',
+                    cuisine: 'Française'
+                },
+                {
+                    nom: 'EL Cafeo',
+                    cuisine: 'Italienne'
+                },
+                {
+                    nom: 'Strabucks',
+                    cuisine: 'Américaine'
+                */ }
+            ],
+            name: '',
+            cuisine: '',
+            nbRestaurants:0,
+            //pagesize:10,
+            page:0,
+            nbRestaurantsParPage: 5,
+            nomRecherche: "",
+            id:""
+        },
+        
+        mounted() {
+            console.log("AVANT AFFICHAGE");
+            this.getRestaurantsFromServer();
+        },
+        methods: {
+            getRestaurantsFromServer() {
+                let url = "http://localhost:8080/api/restaurants?page=" +
+                    this.page +  "&name=" + this.nomRecherche +  "&pagesize=" + //this.pagesize;
+                    this.nbRestaurantsParPage;
+
+                    //gestion des disabled pour les boutons de pagination
+if (this.page==0){
+    document.getElementById("PremPage").disabled='true';
+    document.getElementById("PagePrec").disabled='true';
+}
+    else {
+        document.getElementById("PremPage").disabled='';
+    document.getElementById("PagePrec").disabled='';
+    }
+
+                console.log("Je vais chercher les restaurants sur : " + url)
+
+                fetch(url)
+                    .then((responseJS) => {
+                        //console.log("reponse json");
+                         responseJS.json()
+                    
+                    .then((responseJS) => {
+                        // ici on a une réponse en JS
+                        console.log("J'ai récupéré les restaurants");
+                        this.restaurants = responseJS.data;
+                        this.nbRestaurants = responseJS.count;
+                       
+                    });
+                })
+                    .catch((err) => {
+                        console.log("Une erreur est intervenue " + err);
+                    });
+            },
+
+            searchRestaurantsFromServer: _.debounce(
+                function () {
+                    
+                    this.getRestaurantsFromServer();
+                }, 300),
+
+
+
+    supprimerRestaurant(event) {
+                //this.restaurants.splice(index, 1);
+            
+    event.preventDefault();
+
+    // Récupération du formulaire. Pas besoin de document.querySelector
+    // ou document.getElementById puisque c'est le formulaire qui a généré
+    // l'événement
+    let form = event.target;
+ 
+    let id = form.id.value; // on peut aller chercher la valeur
+                             // d'un champs d'un formulaire
+                             // comme cela, si on connait le nom
+                             // du champ (valeur de son attribut name)
+                             let url = "http://localhost:8080/api/restaurants/" + id;
+
+                             fetch(url, {
+                                 method: "DELETE",
+                             })
+                             .then(function(responseJSON) {
+                                 responseJSON.json()
+                                     .then(function(res) {
+                                         // Maintenant res est un vrai objet JavaScript
+                                         console.log("Restaurant supprimé");
+                                     });
+                                 })
+                                 .catch(function (err) {
+                                     console.log(err);
+                             });
+                         },
+
+
+            ajouterRestaurant(event) {
+                // eviter le comportement par defaut
+             /*   event.preventDefault();
+
+                this.restaurants.push(
+                    {
+                        name: this.name,
+                        cuisine: this.cuisine
+                    }
+                );
+                this.name = "";
+                this.cuisine = "";
+            },*/
+                 event.preventDefault();
+                // Récupération des valeurs des champs du formulaire
+                // en prévision d'un envoi multipart en ajax/fetch
+                let form = event.target;
+                let donneesFormulaire = new FormData(form);
+
+                let url = "http://localhost:8080/api/restaurants";
+
+                fetch(url, {
+                        method: "POST",
+                        body: donneesFormulaire
+                    })
+                    .then((responseJSON) => {
+                        responseJSON.json()
+                            .then((res) => { // arrow function préserve le this
+                                // Maintenant res est un vrai objet JavaScript
+                                console.log("Restaurant ajouté");
+                                this.getRestaurantsFromServer();
+
+                                // remettre le formulaire à zéro
+                                this.name = "";
+                                this.cuisine = "";
+                            });
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
+        },
+        modifierRestaurant(event) {
+
+            event.preventDefault();
+    // Récupération du formulaire. Pas besoin de document.querySelector
+    // ou document.getElementById puisque c'est le formulaire qui a généré
+    // l'événement
+    let form = event.target;
+    // Récupération des valeurs des champs du formulaire
+    // en prévision d'un envoi multipart en ajax/fetch
+    let donneesFormulaire = new FormData(event.target);
+
+    let id = form._id.value; // on peut aller chercher la valeur
+                             // d'un champs d'un formulaire
+                             // comme cela, si on connait le nom
+                             // du champ (valeur de son attribut name)
+
+    let url = "http://localhost:8080/api/restaurants/" + id;
+
+    fetch(url, {
+        method: "PUT",
+        body: donneesFormulaire
+    })
+    .then(function(responseJSON) {
+        responseJSON.json()
+            .then(function(res) {
+                // Maintenant res est un vrai objet JavaScript
+                afficheReponsePUT(res);
+            });
+        })
+        .catch(function (err) {
+            console.log(err);
+    });
+},
+
+            pagePrecedente() {
+                if (this.page > 0) {
+                    this.page--;
+                    this.getRestaurantsFromServer();
+                }
+            },
+            premierePage(){
+                if (this.page > 0) {
+                this.page=0;
+                this.getRestaurantsFromServer();
+                }
+            },
+           dernierePage(){
+                this.page++;
+                this.getRestaurantsFromServer();
+            },
+            pageSuivante() {
+
+                
+                this.page++;
+                this.getRestaurantsFromServer();
+            
+            },
+            changePageSize() {
+                this.getRestaurantsFromServer();
+            },
+            PreModifier(index){
+                // on va chercher tous les éléments du tableau pour les modifier
+                document.getElementByName("_id").value=document.getElementById("dataTable").value[index,0];
+                document.getElementByName("NomMod").value=document.getElementById("dataTable").value[index,1];
+                 document.getElementByName("CuisMod").value=document.getElementById("dataTable").value[index,2];
+},
+getColor(index) {
+    return (index % 2) ? 'lightBlue' : 'pink';
+}
+        }
+    })
+}
